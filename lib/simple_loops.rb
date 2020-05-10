e722cc4cfc2648fdc3bbe9a3cc7e5cf79cf215e8
zara ahmadi <zatokey@gmail.com>
2020-05-10 03:35:25 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
 # Write your methods here
+
+def loop_message_five_times(arr)
+ 5.times do
+  puts arr
+end
+end
+
+def loop_message_n_times(arr,n)
+  n.times do
+    puts arr
+  end
+end
+
+def output_array(arr)
+  arr.each do |i|
+    puts i
+  end
+end
+
+def return_string_array(arr)
+  arr.map { |n| n.to_s }
+end
